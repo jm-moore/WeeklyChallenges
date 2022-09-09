ff68b46e753404e2e85e94ac490be6b4d9af26cf
jm-moore <jmmoore9358@gmail.com>
2022-09-09 09:42:29 -0500

    finished challange set 3

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +8,100 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            foreach (var v in vals)
+            {
+                if (v == false)
+                {
+                    return true;
+                }
+                
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return false;
+            }
+
+            if (numbers.Sum() % 2 != 0)
+            {
+                return true;
+            } else return false;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            bool isUpper = false;
+            bool IsLower = false;
+            bool isNumber = false;
+            foreach (var ch in password)
+            {
+                if (char.IsUpper(ch))
+                {
+                    isUpper = true;
+                }
+                if (char.IsLower(ch))
+                {
+                    IsLower = true;
+                }
+                if (char.IsNumber(ch))
+                {
+                    isNumber = true;
+                }
+            }
+            if (IsLower && isUpper && isNumber)
+            {
+                return true;
+            }
+            else return false;
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+                return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[val.Length - 1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+            }
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            var oddsBelow100 = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                if (i < 100 & i % 2 != 0)
+                {
+                    oddsBelow100.Add(i);
+                }
+            }
+            return oddsBelow100.ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                 words[i] = words[i].ToUpper();
+            }
         }
     }
 }
